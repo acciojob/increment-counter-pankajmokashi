00342fa52c5364f8391f05761aa301d31be824cb
Pankaj Mokashi <127533657+pankajmokashi@users.noreply.github.com>
2023-04-24 18:44:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let p = document.getElementById("p")
+let button = document.getElementById("incrementBtn")
+let count = 0
+function addCount(){
+	alert(p.innerText)
+    p.innerText = ""
+	p.innerText = count + 1
+    count += 1
+}
+button.addEventListener("click",addCount)
